bb8c614b4fc1c72d82d8fd5846c2c4154611a0b1
Nathan Davison <ndavison85@gmail.com>
2015-04-25 21:36:35 -0400

    Made the describe()'s alphabetical.

--- test/unit/models/Menu.test.js
+++ test/unit/models/Menu.test.js
@@ -4,15 +4,6 @@
 
 describe('Menu model', function() {
 
-    describe('create()', function () {
-        it('should create a menu without error.', function(done) {
-            Menu.create({title: "A test menu", owner: "Joey Joe Joe"}).exec(function(err) {
-                if (err) throw err;
-                done();
-            });
-        });
-    });
-    
     describe('beforeCreate()', function() {
         it('should auto generate an id string for menus on creation.', function(done) {
             Menu.create({title: "A test menu", owner: "Joey Joe Joe"}).exec(function(err, created) {
@@ -23,6 +14,15 @@ describe('Menu model', function() {
         });
     });
     
+    describe('create()', function () {
+        it('should create a menu without error.', function(done) {
+            Menu.create({title: "A test menu", owner: "Joey Joe Joe"}).exec(function(err) {
+                if (err) throw err;
+                done();
+            });
+        });
+    });
+        
     describe('destroy()', function() {
         it('should delete a menu without error.', function(done) {
             Menu
